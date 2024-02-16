4c9d382830d7a4fce5117766257cac41590c2882
Pravin S <you@example.com>
2024-02-16 14:31:33 +0100

    installed axios and add jsx in Daily

--- src/components/Daily.jsx
+++ src/components/Daily.jsx
@@ -1,9 +1,24 @@
 import React from "react";
-
+import { useState, useEffect } from "react";
 
 function Daily() {
+
     return (
         <>
+        <section>
+           <div>
+            <img></img>
+            <div>
+                <span>Day</span>
+                <span>Hour</span>
+            </div>
+            <div>
+                <span>wind speed</span>
+                <span>rain</span>
+            </div>
+            <p>weather(sunny or rainy)</p>
+            </div> 
+        </section>
         </>
     )
 }
