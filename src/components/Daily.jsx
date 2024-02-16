96f2ba17160bf42b4d4167c7abc93859a7074c30
Pravin S <you@example.com>
2024-02-16 17:18:00 +0100

    implemented api in daily

--- src/components/Daily.jsx
+++ src/components/Daily.jsx
@@ -1,26 +1,46 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import axios from 'axios';
 
 function Daily() {
 
+    const [dailyForecast, setDailyForecast] = useState();
+    const baseUrl = "https://weatherapi.adaptable.app"
+    const locationId = 1;
+
+    useEffect(() => {
+        axios.get(`${baseUrl}/locations/${locationId}?_embed=daily`)
+            .then((response) => {
+                setDailyForecast(response.data);    
+            })
+            .catch((e) => {
+                console.log(e);
+            })
+    },[]);
+
+    const arr = dailyForecast;
+    console.log(arr)
+  
     return (
         <>
-        <section>
-           <div>
-            <img></img>
-            <div>
-                <span>Day</span>
-                <span>Hour</span>
-            </div>
-            <div>
-                <span>wind speed</span>
-                <span>rain</span>
-            </div>
-            <p>weather(sunny or rainy)</p>
-            </div> 
-        </section>
+        
+            {/* <section>
+                {dailyForecast.daily.map((daily)=>{
+                <div>
+                    <img></img>
+                    <div>
+                        <span>{daily.date}</span>
+                        <span>{daily.hour}</span>
+                    </div>
+                    <div>
+                        <span>{daily.windspeed}</span>
+                        <span>{daily.rain}</span>
+                    </div>
+                    <p>weather(sunny or rainy)</p>
+                </div>})}
+            </section> */}
         </>
-    )
+    );
 }
 
 export default Daily;
