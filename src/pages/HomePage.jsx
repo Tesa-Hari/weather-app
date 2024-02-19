dd5bdc0d2ea83d10c41f40f89c0e16ea74339f33
Tesa Musa <kujtesamusa1@gmail.com>
2024-02-19 14:17:23 +0100

    Update HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,6 +7,7 @@ import logo from '../assets/images/cast.png'
 import Banner from "./BannerPage";
 import Location from "../components/Location";
 import Footer from "./Footer";
+import Weekly from "../components/Weekly";
 function HomePage() {
     const [locationId, setLocationId] = useState(1);
     function selectLocation(id) {
@@ -16,31 +17,21 @@ function HomePage() {
 
     return (
         <>
-       
-       <nav className="flex flex-wrap items-center justify-center sm:justify-start">
-      <img src= "{logo}"alt="Logo" className="h-8 mr-4" /> {/* Adjust height and margin as needed */}
-      <NavLink to="/" className="text-sm font-semibold leading-6 text-gray-900 mr-4">Home | </NavLink>
-      <NavLink to="/about" className="text-sm font-semibold leading-6 text-gray-900 mr-4">About | </NavLink>
-      <NavLink to="/daily" className="text-sm font-semibold leading-6 text-gray-900 mr-4">Daily | </NavLink>
-      <NavLink to="/weekly" className="text-sm font-semibold leading-6 text-gray-900">Weekly</NavLink>
-    </nav>
-    <Location callBackLocation={selectLocation} />
-    <Banner  >
 
-</Banner>
-<Daily locationId={locationId} />
-   
-  
-  
-        {/* Navbar */}
-        {/* Location button with all locations */}
-         <Daily />
-         {/* Weekly */}
-         {/* Footer */}
-         <h1>Homepage is rendered</h1> 
+            <nav className="flex flex-wrap items-center justify-center sm:justify-start">
+                <img src="{logo}" alt="Logo" className="h-8 mr-4" /> {/* Adjust height and margin as needed */}
+                <NavLink to="/" className="text-sm font-semibold leading-6 text-gray-900 mr-4">Home | </NavLink>
+                <NavLink to="/about" className="text-sm font-semibold leading-6 text-gray-900 mr-4">About | </NavLink>
+            </nav>
+
+            <Location callBackLocation={selectLocation} />
+
+            <Banner></Banner>
+            <Daily locationId={locationId} />
+            <Weekly locationId={locationId} />
+
          <Footer />
         </>
     )
 }
-
 export default HomePage;
