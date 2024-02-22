b0cc0bf971adc5d3cacf574c9b630f3134cf038f
Tesa Musa <kujtesamusa1@gmail.com>
2024-02-22 20:23:17 +0100

    Add new section and fix the cards style

--- src/components/CityWeather.jsx
+++ src/components/CityWeather.jsx
@@ -4,33 +4,47 @@ import newdelhi from '../assets/images/NewDelhi.jpg'
 import tokio from '../assets/images/Tokio.jpg'
 import prishtina from '../assets/images/Kosova.jpg'
 import tirana from '../assets/images/Tirana.jpg'
+import munich from '../assets/images/Munich.jpg'
 
 
 
 
 function CityWeather(){
 
-    const cityArray = ['New York','Tokio','New Delhi','Tirana','Prishtina'];
-    const cityTemp = ['12','16','30','15','10'];
-    const cityimages = [newyork,tokio,newdelhi,tirana,prishtina];
+    const cityArray = ['New York','Tokio','New Delhi','Tirana','Prishtina','Munich'];
+    const cityTemp = ['12','16','30','15','10','8'];
+    const cityimages = [newyork,tokio,newdelhi,tirana,prishtina,munich];
     return(
         <>
-         <section className="overflow-x-auto w-screen m-5 p-2 text-lg flex flex-row justify-evenly sm:flex-nowrap py-1 ">
-             
-                {cityArray && cityArray.map((city,index) => {
-                    return <div key={index} style={{backgroundImage: `url(${cityimages[index]})`}}  className="object-fill m-2 mr-8 pt-4 shrink-0 h-96 hover:text-white  text-gray-400 rounded-full w-60 bg-gradient-to-t hover:from-purple-600 hover:to-purple-600 " >
-                       
-                        <div className="flex flex-row justify-evenly m-2">
-                            <span className="bg-purple-500 text-white p-2 rounded-2xl">{city}</span>
-                        </div>
-                        <p className="text-5xl text-purple-600 hover:text-white text-center m-2">{cityTemp[index]}°</p>
-                       
-                    
 
-                      
-                    </div>
-                })}
-            </section>
+
+<section className="bg-gray-50 mb-5">
+<h2 className="text-center p-5 text-2xl font-bold text-purple-400 sm:text-xl">Explore Top Destinations Recommended by Our Weather Forecast</h2>
+  <div className="mx-auto max-w-[1340px] px-4 py-12 sm:px-6 lg:me-0 lg:py-16 lg:pe-0 lg:ps-8 xl:py-24">
+    <div className="grid grid-cols-1 gap-8 lg:grid-cols-3 lg:items-center lg:gap-16">
+      {cityArray.map((city, index) => (
+        <div key={index} className="-mx-6 lg:mx-0">
+          <div className="keen-slider__slide">
+            <blockquote className="flex h-full flex-col justify-between bg-white p-6 shadow-sm sm:p-8 lg:p-12">
+              <div>
+                <div className="flex gap-0.5 text-green-500">
+                </div>
+                <div className="mt-4">
+                  <p className="text-2xl font-bold text-purple-400 sm:text-xl">{city}</p>
+                  <img src={cityimages[index]} alt={city} className="w-full mt-4 h-auto" />
+                  <p className="mt-4 leading-relaxed text-violet-300">
+                    {cityTemp[index]}°
+                  </p>
+                </div>
+              </div>
+            </blockquote>
+          </div>
+        </div>
+      ))}
+    </div>
+  </div>
+</section>
+           
 
         </>
     )
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -10,7 +10,7 @@ function AboutPage() {
          <h1 className="text-3xl text-violet-600 mb-8 text-center hover:text-purple">About Us</h1>
          
        {/* Grid - Cards */}
-       <div className="bg-gradient-to-r from-violet-500 via-purple-400 to-violet-100 h-5/6">
+       <div className="bg-gradient-to-r from-violet-500">
             <div className="max-w-4xl mx-auto p-4">
                 {/* Grid - Cards */}
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-4" >
@@ -24,7 +24,7 @@ function AboutPage() {
                             <h5 className="text-2xl md:text-3xl font-medium">Hari M</h5>
                             <p className="text-slate-500 text-lg mt-2">Hari is a passionate web developer with a keen interest in building intuitive and user-friendly applications. With years of experience in frontend development, Hari specializes in React.js and enjoys experimenting with new technologies to create innovative solutions.</p>
                         </div>
-                        <a href="#" className="text-center bg-violet-300 text-violet-700 py-2 rounded-lg font-semibold mt-4 hover:bg-violet-300 focus:scale-95 transition-all duration-200 ease-out">Explore</a>
+                        <a href="https://github.com/Haripriya2327" className="text-center bg-violet-300 text-violet-700 py-2 rounded-lg font-semibold mt-4 hover:bg-violet-300 focus:scale-95 transition-all duration-200 ease-out">GitHub</a>
                     </div>
 
                     {/* Card 2 */}
@@ -34,9 +34,9 @@ function AboutPage() {
                         </div>
                         <div>
                             <h5 className="text-2xl md:text-3xl font-medium">Tesa M</h5>
-                            <p className="text-slate-500 text-lg mt-2">Tesa is a dedicated software engineer with a passion for crafting robust and efficient web applications. With expertise in both frontend and backend development, Tesa excels in building scalable and performant solutions using modern technologies.</p>
+                            <p className="text-slate-500 text-lg mt-2">Tesa is a Full-Stack dev with a passion for crafting robust and efficient web applications. She finds solace in the pages of a captivating book, cherishes the tranquility of her daily walks, and holds an unyielding affection for her beloved cat, Toby..</p>
                         </div>
-                        <a href="https://github.com/YourGitHubUsername" target="_blank" rel="noopener noreferrer" className="text-center bg-violet-300 text-violet-700 py-2 rounded-lg font-semibold mt-4 hover:bg-violet-300 focus:scale-95 transition-all duration-200 ease-out">Explore</a>
+                        <a href="https://github.com/TesaMusa" target="_blank" rel="noopener noreferrer" className="text-center bg-violet-300 text-violet-700 py-2 rounded-lg font-semibold mt-4 hover:bg-violet-300 focus:scale-95 transition-all duration-200 ease-out">GitHub</a>
                     </div>
 
                 </div>
