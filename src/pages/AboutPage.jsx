1778942725dc8e565563d4fad9233c1e56117b3d
Tesa Musa <kujtesamusa1@gmail.com>
2024-02-22 10:33:43 +0100

    Add About an Footer page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,9 +1,47 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function AboutPage() {
     return (
         <>
-        <p>You are in about Page</p>
+       
+        
+         <NavLink to="/" className="text-2xl text-violet-600 block mb-4 hover:text-purple">Home</NavLink>
+         <h1 className="text-3xl text-violet-600 mb-8 text-center hover:text-purple">About Us</h1>
+         
+       {/* Grid - Cards */}
+       <div className="bg-gradient-to-r from-violet-500 via-purple-400 to-violet-100 h-5/6">
+            <div className="max-w-4xl mx-auto p-4">
+                {/* Grid - Cards */}
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4" >
+
+                    {/* Card 1 */}
+                    <div className="p-6 bg-white rounded-3xl flex flex-col justify-between">
+                        <div className="rounded-3xl overflow-hidden mb-4">
+                            <img src="https://images.pexels.com/photos/301599/pexels-photo-301599.jpeg?cs=srgb&dl=pexels-pixabay-301599.jpg&fm=jpg" alt="Person 1" />
+                        </div>
+                        <div>
+                            <h5 className="text-2xl md:text-3xl font-medium">Hari M</h5>
+                            <p className="text-slate-500 text-lg mt-2">Hari is a passionate web developer with a keen interest in building intuitive and user-friendly applications. With years of experience in frontend development, Hari specializes in React.js and enjoys experimenting with new technologies to create innovative solutions.</p>
+                        </div>
+                        <a href="#" className="text-center bg-violet-300 text-violet-700 py-2 rounded-lg font-semibold mt-4 hover:bg-violet-300 focus:scale-95 transition-all duration-200 ease-out">Explore</a>
+                    </div>
+
+                    {/* Card 2 */}
+                    <div className="p-6 bg-white rounded-3xl flex flex-col justify-between">
+                        <div className="rounded-3xl overflow-hidden mb-4">
+                            <img src="https://images.pexels.com/photos/301599/pexels-photo-301599.jpeg?cs=srgb&dl=pexels-pixabay-301599.jpg&fm=jpg" alt="Person 2" />
+                        </div>
+                        <div>
+                            <h5 className="text-2xl md:text-3xl font-medium">Tesa M</h5>
+                            <p className="text-slate-500 text-lg mt-2">Tesa is a dedicated software engineer with a passion for crafting robust and efficient web applications. With expertise in both frontend and backend development, Tesa excels in building scalable and performant solutions using modern technologies.</p>
+                        </div>
+                        <a href="https://github.com/YourGitHubUsername" target="_blank" rel="noopener noreferrer" className="text-center bg-violet-300 text-violet-700 py-2 rounded-lg font-semibold mt-4 hover:bg-violet-300 focus:scale-95 transition-all duration-200 ease-out">Explore</a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
         </>
     )
 
--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,10 +1,70 @@
 import React from "react";
 
-function Footer(){
-    return(<>
+function Footer() {
+    return (
+        <>
 
-    <h2>Thi is the Footer</h2>
-    </>)
+            <div className=" bg-purple-500">
+                <div className="max-w-2xl mx-auto text-white py-10">
+                    <div className="text-center">
+                        <h3 className="text-3xl mb-3"> Weather App </h3>
+                        {/* weather icon */}
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
+                        </svg>
+                       
+                        <p> Check the Weather before going out. </p>
+                        <div className="flex flex-row justify-between my-10">
+                            <div className="flex items-center border w-auto rounded-lg px-4 py-2 w-52 mx-2">
+                                {/* <img src="https://cdn-icons-png.flaticon.com/512/888/888857.png" className="w-7 md:w-8"> */}
+                                <div className="text-left ml-3">
+                                    <p className='text-xs text-gray-200'>Check Tesa's GitHub
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            class="h-5 w-5"
+                                            fill="currentColor"
+                                            viewBox="0 0 24 24">
+                                            <path
+                                                d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+                                        </svg> </p>
+                                    <p className="text-sm md:text-base"> Tesa </p>
+                                </div>
+                            </div>
+                            <div className="flex items-center border w-auto rounded-lg px-4 py-2 w-44 mx-2">
+                                {/* <img src="https://cdn-icons-png.flaticon.com/512/888/888841.png" className="w-7 md:w-8"> */}
+                                <div className="text-left ml-3">
+                                    <p className='text-xs text-gray-200'>
+                                        Check  Hari's GitHub
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            class="h-5 w-5"
+                                            fill="currentColor"
+                                            viewBox="0 0 24 24">
+                                            <path
+                                                d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+                                        </svg>
+                                    </p>
+                                    <p className="text-sm md:text-base"> Hari </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* <div classNameName="mt-28 flex flex-col md:flex-row md:justify-between items-center text-sm text-gray-400">
+            <p classNameName="order-2 md:order-1 mt-8 md:mt-0"> &copy; Beautiful Footer, 202. </p>
+            <div classNameName="order-1 md:order-2">
+                <span className="px-2">About us</span>
+                <span className="px-2 border-l">Contact us</span>
+                <span className="px-2 border-l">Privacy Policy</span>
+            </div>
+        </div> */}
+                </div>
+            </div>
+
+
+
+
+        </>
+    )
 }
 
 export default Footer;
